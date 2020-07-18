f0229b18ee3d06e6cb3b4e028c955a5b94a23139
samantrs <https://samantrs@git.rabobank.nl>
2020-07-18 15:14:12 +0200

    Initial Commit

---
+++ src/main/java/com/actuator/springappreadiness/SpringAppReadinessApplication.java
@@ -0,0 +1,13 @@
+package com.actuator.springappreadiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppReadinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppReadinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actuator/springappreadiness/SpringAppReadinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actuator.springappreadiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppReadinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
