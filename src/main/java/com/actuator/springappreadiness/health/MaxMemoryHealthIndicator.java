3168f3cf4573e335273fd058c18e27a2da836c0d
samantrs <https://samantrs@git.rabobank.nl>
2020-07-19 16:09:37 +0200

    ReadinessEndpoint and Customized Health Indicator

---
+++ src/main/java/com/actuator/springappreadiness/health/MaxMemoryHealthIndicator.java
@@ -0,0 +1,21 @@
+package com.actuator.springappreadiness.health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.boot.actuate.health.Status;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MaxMemoryHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+
+        final boolean invalid = Runtime.getRuntime().maxMemory() < (100 * 1024 * 1024);
+        Status status = invalid ? Status.DOWN : Status.UP;
+
+        return Health
+                .status(status)
+                .build();
+    }
+}
